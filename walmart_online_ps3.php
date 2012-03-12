b425ea8ea076ca28d6d2c0d0ffcea32f08c6d5aa
scott w <1upgreenshroom@gmail.com>
2012-03-11 20:21:11 -0500

    xbox 360 price added walmart

--- walmart_online_ps3.php
+++ walmart_online_ps3.php
@@ -1,6 +1,6 @@
 <?
 include('simple_html_dom.php'); 
-$url_part1 = 'http://www.walmart.com/browse/Games/_/N-91cgZ1yzttzlZaq90Zaqce/Ne-aq7g?tab_value=All&path=0%3a2636%3a413799&pref_store=5455&depts=&ref=125866.421648+500524.4292810769&ic=660_0';
+$url_part1 = 'http://www.walmart.com/browse/Games/_/N-91cgZ1yzttzlZaq90Zaqce/Ne-aq7g?tab_value=All&path=0%3a2636%3a413799&pref_store=5455&depts=&ref=125866.421648+500524.4292810769&ic=660';
 $html = new simple_html_dom(); 
 for($i=0; $i<=660; $i+=60){
   $url = $url_part1.$i; 
